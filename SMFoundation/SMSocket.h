4463eb99376a5adb91cba2338f59ccaa08bde0fc
Julien-Pierre Avérous <git@sourcemac.com>
2018-11-01 02:49:35 +0100

    - Bump year.

--- SMFoundation/SMSocket.h
+++ SMFoundation/SMSocket.h
@@ -1,7 +1,7 @@
 /*
  *  SMSocket.h
  *
- *  Copyright 2017 Avérous Julien-Pierre
+ *  Copyright 2018 Avérous Julien-Pierre
  *
  *  This file is part of SMFoundation.
  *
--- SMFoundation/SMSocket.m
+++ SMFoundation/SMSocket.m
@@ -1,7 +1,7 @@
 /*
  *  SMSocket.m
  *
- *  Copyright 2017 Avérous Julien-Pierre
+ *  Copyright 2018 Avérous Julien-Pierre
  *
  *  This file is part of SMFoundation.
  *
